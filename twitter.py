42b51938366289164499061b155e0eb1481c5ed6
musquash <musquash@gmx.de>
2017-02-10 16:44:59 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -5,10 +5,10 @@ from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 
 #consumer key, consumer secret, access token, access secret.
-ckey="2dzaBsO9vpZJavOrlOS5vlM7I"
-csecret="FOIqNgXZv23hoiA30Z9fNJxROdaMBOm6imFVq4iveYwvvuFu3X"
-atoken="89008119-LK2Hr7ix5uCIOQplewGfeMp7259MqyvwURIi7A1Co"
-asecret="JkqdkwgzyG0AlJieX5z5pd42wjoTa15A76RK7tIz5OA2r"
+ckey=""
+csecret=""
+atoken=""
+asecret=""
 
 GEOBOX_GER = [5.0770049095, 47.2982950435, 15.0403900146, 54.9039819757]
 blabla = ['en', 'de']
